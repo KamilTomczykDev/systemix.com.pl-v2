92b35f9edebe5183639abfbff521e360b5af2ef8
Kamil Tomczyk <kamiltomczyk.dev@gmail.com>
2023-12-06 12:13:56 +0100

    Added items-center to SectionTitle.jsx

--- src/components/ui/SectionTitle.jsx
+++ src/components/ui/SectionTitle.jsx
@@ -1,6 +1,6 @@
 function SectionTitle({ children }) {
   return (
-    <h2 className="text-brand-green 4xl:text-4xl flex gap-2 text-4xl font-bold xl:text-3xl">
+    <h2 className="flex items-center gap-2 text-4xl font-bold text-brand-green xl:text-3xl 4xl:text-4xl">
       {children}
     </h2>
   );
